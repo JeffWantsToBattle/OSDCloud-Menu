fc1625cefb3a17d1db3ec1ef3144eb77fcd8364b
JeffWantsToBattle <121041862+JeffWantsToBattle@users.noreply.github.com>
2024-05-05 22:46:15 +0200

    Update OSDCloudStartURL.ps1

--- OSDCloudStartURL.ps1
+++ OSDCloudStartURL.ps1
@@ -1,1 +1,27 @@
-Start-OSDCloudGUI
+$MainMenu = {
+Write-Host " ***************************"
+Write-Host " *         OSDCloud        *"
+Write-Host " ***************************"
+Write-Host
+Write-Host " 1.) Start-OSDCloudGUI"
+Write-Host " 2.) Start-OSDCloudAzure"
+Write-Host " 3.) Quit"
+Write-Host
+Write-Host " Select an option and press Enter: "  -nonewline
+}
+cls
+Do {
+cls
+Invoke-Command $MainMenu
+$Select = Read-Host
+Switch ($Select)
+    {
+    1 {
+        Start-OSDCloudGUI
+       }
+    2 {
+        Start-OSDCloudAzure
+       }
+    }
+}
+While ($Select -ne 3)
