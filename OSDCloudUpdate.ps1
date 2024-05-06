82368e66ae537596c557333bde6900eed5f7b368
JeffWantsToBattle <121041862+JeffWantsToBattle@users.noreply.github.com>
2024-05-06 14:40:44 +0200

    Update OSDCloudUpdate.ps1

--- OSDCloudUpdate.ps1
+++ OSDCloudUpdate.ps1
@@ -6,26 +6,31 @@ $version = "V1.0"
 $disk = Get-WMIObject Win32_Volume | ? { $_.Label -eq 'OSDCloudUSB' }
 $disk = $disk.Name
 
+Write-Host " ***************************"
+Write-Host " *         OSDCloud        *"
+Write-Host " ***************************"
+Write-Host
+
 if ($disk -eq $null) {
-    Write-host "OSDCloudUSB drive not found" -ForegroundColor Red
+    Write-host " OSDCloudUSB drive not found" -ForegroundColor Red
     cmd /c 'pause'
 } else {
-    Write-host "OSDCloudUSB drive found" -ForegroundColor Green
+    Write-host " OSDCloudUSB drive found" -ForegroundColor Green
     ###Check version
     $file = "Version.txt"
     $folder = 'OSDCloud\'
     $location = "$disk$folder"
     $versioncheck = Get-Content "$location$file" -ErrorAction SilentlyContinue
     if ($versioncheck -eq $version){
-        Write-host "OSDCloudUSB already up-to-date" -ForegroundColor Green
+        Write-host " OSDCloudUSB already up-to-date" -ForegroundColor Green
         cmd /c 'pause'
     } else {
-        Write-host "Updating OSDCloudUSB" -ForegroundColor Green
+        Write-host " Updating OSDCloudUSB" -ForegroundColor Green
         ###Write new version
         New-Item -Path $location -Name "$file" -ItemType "file" -Value $version -Force
         #Invoke-WebRequest -Uri https://raw.githubusercontent.com/JeffWantsToBattle/OSD/main/file1 -OutFile .\file1
         #Invoke-WebRequest -Uri https://raw.githubusercontent.com/JeffWantsToBattle/OSD/main/file2 -OutFile .\file2
-        Write-host "Updating compleet" -ForegroundColor Green
+        Write-host " Updating compleet" -ForegroundColor Green
         cmd /c 'pause'
 
     }
