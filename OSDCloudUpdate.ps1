cd1e3ed32bfe1be9f2867e91988625b7bc3ed7d6
JeffWantsToBattle <121041862+JeffWantsToBattle@users.noreply.github.com>
2024-05-06 12:16:04 +0200

    Update OSDCloudUpdate.ps1

--- OSDCloudUpdate.ps1
+++ OSDCloudUpdate.ps1
@@ -1,4 +1,25 @@
-$versie = "1.0"
-CLS
-Write-Host "OSDCloud Update $versie"
-cmd /c 'pause'
+#Custom variables
+$version = "V1.0"
+
+#Search OSDCloud disk
+$disk = Get-WMIObject Win32_Volume | ? { $_.Label -eq 'OSDCloudUSB' }
+$disk = $disk.Name
+
+if ($disk -eq "") {
+    Write-host "OSDCloud niet gevonden"
+    cmd /c 'pause'
+} else {
+    #Check version
+    $file = "versie.txt"
+    $versioncheck = Get-Content $disk$file -ErrorAction SilentlyContinue
+
+    if ($versioncheck -eq $version){
+        Write-host "OSDCloud is up-to-date"
+        cmd /c 'pause'
+    } else {
+        Write-host "updating OSDCloud"
+
+        #Write new version
+        New-Item -Path $disk -Name "versie.txt" -ItemType "file" -Value $versie -Force
+    }
+}
