8954f85eb9bbf690eccf942d86f4a90d1614fa41
JeffWantsToBattle <121041862+JeffWantsToBattle@users.noreply.github.com>
2024-05-06 13:07:42 +0200

    Update OSDCloudUpdate.ps1

--- OSDCloudUpdate.ps1
+++ OSDCloudUpdate.ps1
@@ -9,17 +9,17 @@ if ($disk -eq $null) {
     Write-host "OSDCloudUSB drive not found"
     cmd /c 'pause'
 } else {
-    Write-host "OSDCloudUS drive found"
+    Write-host "OSDCloudUSB drive found"
     #Check version
     $file = "Version.txt"
     $folder = 'OSDCloud\'
     $location = "$disk$folder"
     $versioncheck = Get-Content "$location$file" -ErrorAction SilentlyContinue
     if ($versioncheck -eq $version){
-        Write-host "OSDCloud is up-to-date"
+        Write-host "OSDCloudUSB already up-to-date"
         cmd /c 'pause'
     } else {
-        Write-host "Updating OSDCloud"
+        Write-host "Updating OSDCloudUSB"
         #Write new version
         New-Item -Path $location -Name "$file" -ItemType "file" -Value $version -Force
         Write-host "Updating compleet"
