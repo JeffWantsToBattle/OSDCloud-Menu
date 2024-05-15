a9e68e3b52a0de70022b8c2ed4e73a870281502e
JeffWantsToBattle <121041862+JeffWantsToBattle@users.noreply.github.com>
2024-05-15 21:59:20 +0200

    Update OSDCloudUpdateMenu.ps1

--- OSDCloudUpdateMenu.ps1
+++ OSDCloudUpdateMenu.ps1
@@ -1,11 +1,25 @@
+###Intro formatting
 Clear-Host
 Write-Host " ***************************"
 Write-Host " *   OSDCloud Update menu  *"
 Write-Host " ***************************"
 Write-Host
 
+###Search OSDCloud disk
 $disk = Get-WMIObject Win32_Volume | Where-Object { $_.Label -eq 'OSDCloudUSB' }
 $disk = $disk.Name
+
+###Getting version from .\Update\Version.txt
+$version = Invoke-WebRequest -Uri https://raw.githubusercontent.com/JeffWantsToBattle/OSD/main/Update/Version.txt
+$version = $version.Content.Split([Environment]::NewLine) | Select -First 1
+
+###Getting version from OSDCloudUSB drive
+$file = "Version.txt"
+$folder = 'OSDCloud\'
+$location = "$disk$folder"
+$versionondisk = Get-Content "$location$file" -ErrorAction SilentlyContinue
+
+###Check if OSDCloudUSB drive is found
 if ($disk -eq $null) {
     Write-host " OSDCloudUSB drive not found" -ForegroundColor Red
     Write-host " Check that the partition name matches: OSDCloudUSB" -ForegroundColor Red
@@ -17,8 +31,15 @@ if ($disk -eq $null) {
     Write-Host " ***************************"
     Write-Host " *   OSDCloud Update menu  *"
     Write-Host " ***************************"
-    Write-Host
-    Write-Host " 1.) Update OSDCloudUSB"
+    Write-Host " OSDCloud version $versionondisk found on $disk" -nonewline
+    if ($versionondisk -lt $version) {
+        Write-Host "$versionondisk" -nonewline -ForegroundColor Red
+        Write "found on $disk"
+    } else {
+        Write-Host "$versionondisk" -nonewline -ForegroundColor green
+        Write "found on $disk"
+    }
+    Write-Host " 1.) Update OSDCloudUSB to $version"
     Write-Host " 2.) Update WinPE < not ready"
     Write-Host " 3.) Update Powershell scripts"
     Write-Host " 4.) Download Windows 11 23H2 Retail"
