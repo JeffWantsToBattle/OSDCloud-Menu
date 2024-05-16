d8730178a5540a8500ccb3a830ac7e2eb3b00843
JeffWantsToBattle <121041862+JeffWantsToBattle@users.noreply.github.com>
2024-05-16 22:22:02 +0200

    Update OSDCloudStartURL.ps1

--- OSDCloudStartURL.ps1
+++ OSDCloudStartURL.ps1
@@ -8,7 +8,7 @@ Write-Host " 1.) OSDCloud Local"
 Write-Host " 2.) OSDCloud Azure"
 Write-Host " 3.) OSDCloud Azure Sandbox"
 Write-Host " 4.) Autopilot"
-Write-Host " 5.) Update OSDCloudUSB (Menu)"
+Write-Host " 5.) Install/Update OSDCloudUSB (Menu)"
 Write-Host " Q.) Exit Menu"
 Write-Host
 Write-Host " Select an option and press Enter: "  -nonewline
@@ -36,7 +36,7 @@ Switch ($Select)
         } Catch {
             Clear-Host
             Write-Host " ***************************"
-            Write-Host " *         OSDCloud        *"
+            Write-Host " *         Autopilot       *"
             Write-Host " ***************************"
             Write-Host
             Write-Host "Autopilot script not found, installing script"
