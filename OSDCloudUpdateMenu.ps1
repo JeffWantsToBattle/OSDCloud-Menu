6e98ba88de88c95b48654010610ed9396a7627a1
JeffWantsToBattle <121041862+JeffWantsToBattle@users.noreply.github.com>
2024-05-16 22:20:12 +0200

    Update OSDCloudUpdateMenu.ps1

--- OSDCloudUpdateMenu.ps1
+++ OSDCloudUpdateMenu.ps1
@@ -2,34 +2,31 @@
 $disk = Get-WMIObject Win32_Volume | Where-Object { $_.Label -eq 'OSDCloudUSB' }
 $disk = $disk.Name
 
-###Getting version from .\Update\Version.txt
+###Getting version from .\Update\Version.txt and .\Update\VersionWinPE.txt
 $version = Invoke-WebRequest -Uri https://raw.githubusercontent.com/JeffWantsToBattle/OSD/main/Update/Version.txt
 $version = $version.Content.Split([Environment]::NewLine) | Select -First 1
+$versionWinPE = Invoke-WebRequest -Uri https://raw.githubusercontent.com/JeffWantsToBattle/OSD/main/Update/VersionWinPE.txt
+$versionWinPE = $versionWinPE.Content.Split([Environment]::NewLine) | Select -First 1
 
-###Getting version from OSDCloudUSB drive
+###Getting OSDCloudUSB and WinPE version from drive
 $file = "Version.txt"
 $folder = 'OSDCloud\'
 $location = "$disk$folder"
 $versionondisk = Get-Content "$location$file" -ErrorAction SilentlyContinue
+$versionWinPEondisk = Get-Content "$location$file" -ErrorAction SilentlyContinue
+
+Clear-Host
+$MainMenu = {
+Write-Host " ***************************"
+Write-Host " *   OSDCloud Update menu  *"
+Write-Host " ***************************"
+Write-Host
 
 ###Check if OSDCloudUSB drive is found
 if ($disk -eq $null) {
-    Clear-Host
-    Write-Host " ***************************"
-    Write-Host " *   OSDCloud Update menu  *"
-    Write-Host " ***************************"
-    Write-Host
     Write-host " OSDCloudUSB drive not found" -ForegroundColor Red
     Write-host " Check that the partition name matches: OSDCloudUSB" -ForegroundColor Red
-    Write-Host
-    cmd /c 'pause'
 } else {
-    Clear-Host
-    $MainMenu = {
-    Write-Host " ***************************"
-    Write-Host " *   OSDCloud Update menu  *"
-    Write-Host " ***************************"
-    Write-Host
     Write-Host " OSDCloudUSB version " -nonewline
     if ($versionondisk -lt $version) {
         Write-Host "$versionondisk " -nonewline -ForegroundColor Red
@@ -38,6 +35,16 @@ if ($disk -eq $null) {
         Write-Host "$versionondisk " -nonewline -ForegroundColor green
         Write-Host "found on $disk"
     }
+    Write-Host " WinPE version " -nonewline
+    if ($versionWinPEondisk -lt $versionWinPE) {
+        Write-Host "$versionWinPEondisk " -nonewline -ForegroundColor Red
+        Write-Host "found on $disk"
+    } else {
+        Write-Host "$versionWinPEondisk " -nonewline -ForegroundColor green
+        Write-Host "found on $disk"
+    }
+}
+    Write-Host
     Write-Host " 1.) Update OSDCloudUSB to version $version"
     Write-Host " 2.) Update WinPE < not ready"
     Write-Host " 3.) Update Powershell scripts"
@@ -72,4 +79,3 @@ if ($disk -eq $null) {
         }
     }
     While ($Select -ne "Q")
-}
