3177eebc906f7a9f53f7953e53559d1610dc696d
JeffWantsToBattle <121041862+JeffWantsToBattle@users.noreply.github.com>
2024-05-17 13:32:11 +0200

    Update OSDCloudDownloadWindows.ps1

--- OSDCloudDownloadWindows.ps1
+++ OSDCloudDownloadWindows.ps1
@@ -10,10 +10,10 @@ Write-Host " ***************************"
 Write-Host
 Write-Host " Select a Windows version:"
 
-$Menu = @{}
+$MenuVer = @{}
 $WinVer | ForEach-Object -Begin {$i = 1} { 
      Write-Host " $i.) $_" 
-     $Menu.add("$i",$_)
+     $MenuVer.add("$i",$_)
      $i++
 }
 
@@ -21,8 +21,27 @@ Write-Host " Q.) Back"
 
 $WinVerSelection = Read-Host " Select an option and press Enter"
 
-if ($WinVerSelection -eq 'Q') { 
-     Invoke-WebPSScript 'https://raw.githubusercontent.com/JeffWantsToBattle/OSD/main/OSDCloudStartURL.ps1'
+Clear-Host
+Write-Host " ***************************"
+Write-Host " *         OSDCloud        *"
+Write-Host " ***************************"
+Write-Host
+Write-Host " Select a Windows language:"
+
+$MenuLang = @{}
+$WinLang | ForEach-Object -Begin {$i = 1} { 
+     Write-Host " $i.) $_" 
+     $MenuLang.add("$i",$_)
+     $i++
+}
+
+Write-Host " Q.) Back"
+
+$WinLangSelection = Read-Host " Select an option and press Enter"
+
+if ($WinLangSelection -eq 'Q') { 
+     Return 
 } Else { 
-     Update-OSDCloudUSB -OSName $Menu.$WinVerSelection -OSLanguage nl-nl -OSLicense Retail
+     Clear-Host
+     Update-OSDCloudUSB -OSName $MenuVer.$WinVerSelection -OSLanguage $menuLang.$WinLangSelection -OSLicense Retail
 }
