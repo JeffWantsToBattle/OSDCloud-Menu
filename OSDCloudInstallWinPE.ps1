fe873af803470e131c0176d66e2a6cd167314a91
JeffWantsToBattle <121041862+JeffWantsToBattle@users.noreply.github.com>
2024-05-17 22:26:51 +0200

    Update OSDCloudInstallWinPE.ps1

--- OSDCloudInstallWinPE.ps1
+++ OSDCloudInstallWinPE.ps1
@@ -3,6 +3,7 @@ Write-Host " ***************************"
 Write-Host " *    WinPE Installation   *"
 Write-Host " ***************************"
 Write-Host
+
 New-OSDCloudUSB -fromIsoUrl 'https://jvdosd.blob.core.windows.net/bootimage/OSDCloud_NoPrompt.iso'
 New-Item -ItemType Directory -Path $location\Automate | Out-Null
 Invoke-WebRequest -Uri https://raw.githubusercontent.com/JeffWantsToBattle/OSD/main/Update/Automate/Start-OSDCloudGUI.json -OutFile $location\Automate\Start-OSDCloudGUI.json
@@ -10,18 +11,36 @@ New-Item -Path $location -Name "$file" -ItemType "file" -Value $version -Force |
 New-Item -Path $location -Name "$fileWinPE" -ItemType "file" -Value $versionWinPE -Force | Out-Null
 New-Item -Path $location -Name "Start-Menu.ps1" -ItemType "file" -Value "iex (irm osd.jevede.nl)" -Force | Out-Null
 
+
+$MainMenu = {
+    Write-Host " ***************************"
+    Write-Host " *    WinPE Installation   *"
+    Write-Host " ***************************"
+    Write-Host
+    Write-Host " WinPE Installation complete"
+    Write-Host " Install one more USB?"
+    Write-Host
+    Write-Host " 1.) Yes (Keeps ISO for next install)"
+    Write-Host " 2.) No (Delete ISO)"
+    Write-Host " Q.) Back (Keeps ISO in Download folder (Not recommanded))"
+    Write-Host
+    Write-Host " Select an option and press Enter: "  -nonewline
+}
 Clear-Host
-Write-Host " ***************************"
-Write-Host " *    WinPE Installation   *"
-Write-Host " ***************************"
-Write-Host
-Write-Host " WinPE Installation complete"
-Write-Host " Install one more USB?"
-Write-Host
-Write-Host " 1.) Yes"
-Write-Host " 2.) No"
-
-
-#Add unmount ISO file
-$ISOPath = (New-Object -ComObject Shell.Application).Namespace('shell:Downloads').Self.Path
-Remove-Item $ISOPath\OSDCloud_NoPrompt.iso -ErrorAction SilentlyContinue | Out-Null
+Do {
+    Clear-Host
+    Invoke-Command $MainMenu
+    $Select = Read-Host
+    Switch ($Select)
+        {
+        1 {
+            Invoke-WebPSScript 'https://raw.githubusercontent.com/JeffWantsToBattle/OSD/main/OSDCloudInstallWinPE.ps1'
+        }
+        2 {
+            $ISOPath = (New-Object -ComObject Shell.Application).Namespace('shell:Downloads').Self.Path
+            Dismount-DiskImage -ImagePath "$ISOPath\OSDCloud_NoPrompt.iso"
+            Remove-Item $ISOPath\OSDCloud_NoPrompt.iso -ErrorAction SilentlyContinue | Out-Null
+        }
+    }
+}
+While ($Select -ne "Q")
