3e0e1217d080209823b21a23d32c13426a58e9c2
JeffWantsToBattle <121041862+JeffWantsToBattle@users.noreply.github.com>
2024-05-17 21:27:18 +0200

    Update OSDCloudDownloadDriver.ps1

--- OSDCloudDownloadDriver.ps1
+++ OSDCloudDownloadDriver.ps1
@@ -1,39 +1,39 @@
 $MainMenu = {
-Write-Host " ***************************"
-Write-Host " *     OSDCloud Drivers    *"
-Write-Host " ***************************"
-Write-Host
-Write-Host " 1.) Download HP Drivers"
-Write-Host " 2.) Download DELL Drivers"
-Write-Host " 3.) Download Lenovo Drivers"
-Write-Host " 4.) Download Microsoft Drivers"
-Write-Host " Q.) Back"
-Write-Host
-Write-Host " Select an option and press Enter: "  -nonewline
+    Write-Host " ***************************"
+    Write-Host " *     OSDCloud Drivers    *"
+    Write-Host " ***************************"
+    Write-Host
+    Write-Host " 1.) Download HP Drivers"
+    Write-Host " 2.) Download DELL Drivers"
+    Write-Host " 3.) Download Lenovo Drivers"
+    Write-Host " 4.) Download Microsoft Drivers"
+    Write-Host " Q.) Back"
+    Write-Host
+    Write-Host " Select an option and press Enter: "  -nonewline
 }
 Clear-Host
 Do {
-Clear-Host
-Invoke-Command $MainMenu
-$Select = Read-Host
-Switch ($Select)
-    {
-    1 {
-        Update-OSDCloudUSB -DriverPack HP
-        Invoke-WebPSScript 'https://raw.githubusercontent.com/JeffWantsToBattle/OSD/main/OSDCloudDriverDownload.ps1'
-    }
-    2 {
-        Update-OSDCloudUSB -DriverPack Dell
-        Invoke-WebPSScript 'https://raw.githubusercontent.com/JeffWantsToBattle/OSD/main/OSDCloudDriverDownload.ps1'
-    }
-    3 {
-        Update-OSDCloudUSB -DriverPack Lenovo
-        Invoke-WebPSScript 'https://raw.githubusercontent.com/JeffWantsToBattle/OSD/main/OSDCloudDriverDownload.ps1'
-    }
-    4 {
-        Update-OSDCloudUSB -DriverPack Microsoft
-        Invoke-WebPSScript 'https://raw.githubusercontent.com/JeffWantsToBattle/OSD/main/OSDCloudDriverDownload.ps1'
-    }
+    Clear-Host
+    Invoke-Command $MainMenu
+    $Select = Read-Host
+    Switch ($Select)
+        {
+        1 {
+            Update-OSDCloudUSB -DriverPack HP
+            Invoke-WebPSScript 'https://raw.githubusercontent.com/JeffWantsToBattle/OSD/main/OSDCloudDriverDownload.ps1'
+        }
+        2 {
+            Update-OSDCloudUSB -DriverPack Dell
+            Invoke-WebPSScript 'https://raw.githubusercontent.com/JeffWantsToBattle/OSD/main/OSDCloudDriverDownload.ps1'
+        }
+        3 {
+            Update-OSDCloudUSB -DriverPack Lenovo
+            Invoke-WebPSScript 'https://raw.githubusercontent.com/JeffWantsToBattle/OSD/main/OSDCloudDriverDownload.ps1'
+        }
+        4 {
+            Update-OSDCloudUSB -DriverPack Microsoft
+            Invoke-WebPSScript 'https://raw.githubusercontent.com/JeffWantsToBattle/OSD/main/OSDCloudDriverDownload.ps1'
+        }
     }
 }
 While ($Select -ne "Q")
