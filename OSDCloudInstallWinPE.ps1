64a7ca8272196ff2bf7f1adf54b2f82325bf8b3e
JeffWantsToBattle <121041862+JeffWantsToBattle@users.noreply.github.com>
2024-05-18 22:56:03 +0200

    Update OSDCloudInstallWinPE.ps1

--- OSDCloudInstallWinPE.ps1
+++ OSDCloudInstallWinPE.ps1
@@ -8,12 +8,12 @@ Write-Host
 
 ###Starting WinPE install from Azure Blob and writing the necessary files
 New-OSDCloudUSB -fromIsoUrl 'https://jvdosd.blob.core.windows.net/bootimage/OSDCloud_NoPrompt.iso'
-New-Item -ItemType Directory -Path $location\Automate | Out-Null
+New-Item -ItemType Directory -Path $location\Automate -force -ErrorAction SilentlyContinue | Out-Null
 Invoke-WebRequest -Uri https://raw.githubusercontent.com/JeffWantsToBattle/OSD/main/Update/Automate/Start-OSDCloudGUI.json -OutFile $location\Automate\Start-OSDCloudGUI.json
 Invoke-WebRequest -Uri https://raw.githubusercontent.com/JeffWantsToBattle/OSD/main/Update/Start-Menu.ps1 -OutFile $location\Start-Menu.ps1
-New-Item -Path "$location" -Name "$file" -ItemType "file" -Value $version -Force | Out-Null
-New-Item -Path "$location" -Name "$fileWinPE" -ItemType "file" -Value $versionWinPE -Force | Out-Null
-New-Item -Path "$disk" -Name "Start-Menu.ps1" -ItemType "file" -Value "Start-Process powershell -Verb runAs "iex (irm osd.jevede.nl)"" -Force | Out-Null
+New-Item -Path "$location" -Name "$file" -ItemType "file" -Value $version -force -ErrorAction SilentlyContinue | Out-Null
+New-Item -Path "$location" -Name "$fileWinPE" -ItemType "file" -Value $versionWinPE -force -ErrorAction SilentlyContinue | Out-Null
+New-Item -Path "$disk" -Name "Start-Menu.ps1" -ItemType "file" -Value "Start-Process powershell -Verb runAs "iex (irm osd.jevede.nl)"" -force -ErrorAction SilentlyContinue | Out-Null
 
 $MainMenu = {
     Write-Host " ***************************"
