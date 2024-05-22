52deeb97689296a85e941c1891c3662530f85dc5
JeffWantsToBattle <121041862+JeffWantsToBattle@users.noreply.github.com>
2024-05-22 19:43:53 +0200

    Update OSDCloudUSBUpdate.ps1

--- OSDCloudUSBUpdate.ps1
+++ OSDCloudUSBUpdate.ps1
@@ -4,14 +4,14 @@ Write-Host " *    Update OSDCloudUSB   *"
 Write-Host " ***************************"
 Write-Host
 
-###Check if OSDCloudUSB drive is found
+### Check if OSDCloudUSB drive is found
 if ($disk -eq $null) {
     Write-host " OSDCloudUSB drive not found" -ForegroundColor Red
     Write-host " Check that the partition name matches: OSDCloudUSB" -ForegroundColor Red
     Write-Host
     cmd /c 'pause'
 } else {
-    ###Version check
+    ### Version check
     if ($versionondisk -eq $version){
         Write-host " OSDCloudUSB already updated to version $version" -ForegroundColor Green
         Write-Host
@@ -19,7 +19,7 @@ if ($disk -eq $null) {
     } else {
         Write-host " Updating OSDCloudUSB to version $version" -ForegroundColor Green
         New-Item -ItemType Directory -Path $location\Automate -force -ErrorAction SilentlyContinue | Out-Null
-        ###Write new version + error handeling
+        ### Write new version + error handeling
         try {
             Invoke-WebRequest -Uri https://raw.githubusercontent.com/JeffWantsToBattle/OSD/main/Update/Automate/Start-OSDCloudGUI.json -OutFile $location\Automate\Start-OSDCloudGUI.json
         } catch {
