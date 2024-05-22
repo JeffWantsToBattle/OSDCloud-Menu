e631976f7c2eb12f4953b2607cbeb4cf2ec7278a
JeffWantsToBattle <121041862+JeffWantsToBattle@users.noreply.github.com>
2024-05-22 19:43:41 +0200

    Update OSDCloudUpdateMenu.ps1

--- OSDCloudUpdateMenu.ps1
+++ OSDCloudUpdateMenu.ps1
@@ -1,16 +1,16 @@
-###Search OSDCloud and WinPE disk
+### Search OSDCloud and WinPE disk
 $disk = Get-WMIObject Win32_Volume | Where-Object { $_.Label -eq 'OSDCloudUSB' }
 $disk = $disk.Name
 $diskwinpe = Get-WMIObject Win32_Volume | Where-Object { $_.Label -eq 'WinPE' }
 $diskwinpe = $diskwinpe.Name
 
-###Getting version from .\Update\Version.txt and .\Update\VersionWinPE.txt
+### Getting version from .\Update\Version.txt and .\Update\VersionWinPE.txt
 $version = Invoke-WebRequest -Uri https://raw.githubusercontent.com/JeffWantsToBattle/OSD/main/Update/Version.txt
 $version = $version.Content.Split([Environment]::NewLine) | Select -First 1
 $versionWinPE = Invoke-WebRequest -Uri https://raw.githubusercontent.com/JeffWantsToBattle/OSD/main/Update/VersionWinPE.txt
 $versionWinPE = $versionWinPE.Content.Split([Environment]::NewLine) | Select -First 1
 
-###Getting OSDCloudUSB and WinPE version from drive
+### Getting OSDCloudUSB and WinPE version from drive
 $file = "Version.txt"
 $fileWinPE = "VersionWinPE.txt"
 $folder = 'OSDCloud\'
@@ -25,9 +25,9 @@ $MainMenu = {
     Write-Host " ***************************"
     Write-Host
     
-    ###Check if OSDCloudUSB drive is found
+    ### Check if OSDCloudUSB drive is found
     if ($disk -eq $null) {
-        ###Menu if disk is not found
+        ### Menu if disk is not found
         Write-host " OSDCloudUSB drive not found" -ForegroundColor Red
         Write-host " Check that the partition name matches: OSDCloudUSB" -ForegroundColor Red
         Write-Host
@@ -38,7 +38,7 @@ $MainMenu = {
         Write-Host " Select an option and press Enter: "  -nonewline
     } else {
         Write-Host " OSDCloudUSB " -nonewline
-        ###Check if OSDCloudUSB version is lower then new version
+        ### Check if OSDCloudUSB version is lower then new version
         if ($versionondisk -lt $version) {
             Write-Host "$versionondisk " -nonewline -ForegroundColor Red
             Write-Host "found on $disk"
@@ -47,7 +47,7 @@ $MainMenu = {
             Write-Host "found on $disk"
         }
         Write-Host " WinPE " -nonewline
-        ###Check if WinPE version is lower then new version
+        ### Check if WinPE version is lower then new version
         if ($versionWinPEondisk -lt $versionWinPE) {
             Write-Host "$versionWinPEondisk " -nonewline -ForegroundColor Red
             Write-Host "found on $diskwinpe"
@@ -55,7 +55,7 @@ $MainMenu = {
             Write-Host "$versionWinPEondisk " -nonewline -ForegroundColor green
             Write-Host "found on $diskwinpe"
         }
-        ###Menu if disk is found
+        ### Menu if disk is found
         Write-Host
         Write-Host " 1.) Install OSDCloudUSB (WinPE)" -nonewline
         Write-Host " $versionWinPE" -ForegroundColor green
