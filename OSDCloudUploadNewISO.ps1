5629bd487c96de118888089b69c67197a04ea5e7
JeffWantsToBattle <121041862+JeffWantsToBattle@users.noreply.github.com>
2024-05-27 22:49:30 +0200

    -

---
+++ OSDCloudUploadNewISO.ps1
@@ -0,0 +1,26 @@
+$downloadsPath = (New-Object -ComObject Shell.Application).Namespace('shell:Downloads').Self.Path
+    Write-Host " ***************************"
+    Write-Host " *         OSDCloud        *"
+    Write-Host " ***************************"
+    Write-Host
+
+
+if (Test-Path -path $env:APPDATA\AzCopy\azcopy.exe -PathType Leaf){
+    ### Starting the AzCopy action
+    Invoke-Expression "& '$env:APPDATA\AzCopy\azcopy.exe' login"
+    try {
+    Invoke-Expression  "& '$env:APPDATA\AzCopy\azcopy.exe' copy '$downloadsPath\Test.txt' https://jvdosd.blob.core.windows.net/bootimage/Test.txt" -ErrorAction SilentlyContinue | Out-Null
+    } catch {
+        Write-Host " Error"
+        cmd /c 'pause'
+    }
+} Else {
+    ### Install AzCopy
+    Invoke-WebRequest -Uri "https://aka.ms/downloadazcopy-v10-windows" -OutFile AzCopy.zip -UseBasicParsing
+    Expand-Archive ./AzCopy.zip ./AzCopy -Force
+    mkdir $env:APPDATA\AzCopy
+    Get-ChildItem ./AzCopy/*/azcopy.exe | Move-Item -Destination $env:APPDATA\AzCopy\
+    Remove-Item -Force AzCopy.zip
+    Remove-Item -Force -Recurse .\AzCopy\
+    # Rerun the script from start?
+}
