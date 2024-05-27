06b2b6a45901f833d411c41fe71e9c4ae7ae9964
JeffWantsToBattle <121041862+JeffWantsToBattle@users.noreply.github.com>
2024-05-27 20:21:54 +0200

    -

--- Test.ps1
+++ Test.ps1
@@ -9,7 +9,7 @@ Write-Host " Downloading WinPE"
 Write-Host
 
 ### Test if any External hard disk media is fount
-$testdisk = GET-WMIOBJECT win32_diskdrive | Where { $_.mediatype -eq 'External hard disk media' }
+$testdisk = GET-WMIOBJECT win32_diskdrive | Where { $_.mediatype -eq 'Removable Media' -or $_.mediatype -eq 'Removable Media' }
 if ( $testdisk -eq $null) {
     Write-Host "No USB Drive found, going back to menu"
     Write-Host
