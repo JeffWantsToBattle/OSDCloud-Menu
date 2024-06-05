39c0e1ee2049060f5cb10ce2dd4ce51e8a963512
JeffWantsToBattle <121041862+JeffWantsToBattle@users.noreply.github.com>
2024-06-05 20:56:12 +0200

    Update OSDCloudStartURL.ps1

--- OSDCloudStartURL.ps1
+++ OSDCloudStartURL.ps1
@@ -1,6 +1,6 @@
 ### Set the repository and ISO download link
-$repositoryURL  = 'https://raw.githubusercontent.com/JeffWantsToBattle/OSDCloud-Menu/main'
-$ISOURL         = 'https://jvdosd.blob.core.windows.net/bootimage/OSDCloud_NoPrompt.iso'
+$RepositoryURL  = '(Replace with RAW repository URL)'
+$ISOURL         = '(Replace with direct ISO download URL)'
 
 ### Install OSDCloud module if not present
 #if (Get-InstalledModule -Name OSD) { < Get-InstalledModule can be slow so replaced with Test-Path, need to test in WinPE
@@ -23,10 +23,7 @@ $MainMenu = {
     Write-Host " ***************************"
     Write-Host
     Write-Host " 1.) OSDCloud Local (WinPE)"
-    Write-Host " 2.) OSDCloud Azure (WinPE)"
-    Write-Host " 3.) OSDCloud Azure Sandbox (WinPE)"
-    Write-Host " 4.) Install/Update OSDCloudUSB (Windows)"
-    Write-Host " 5.) Autopilot (Windows)"
+    Write-Host " 2.) Install/Update OSDCloudUSB (Windows)"
     Write-Host " Q.) Exit Powershell"
     Write-Host
     Write-Host " Select an option and press Enter: "  -nonewline
@@ -42,29 +39,7 @@ Do {
             Start-OSDCloudGUI
         }
         2 {
-            Start-OSDCloudAzure
-        }
-        3 {
-            iex (irm az.osdcloud.com)
-        }
-        4 {
             Invoke-WebPSScript $repositoryURL/OSDCloudUpdateMenu.ps1
-        }
-        5 {
-            Clear-Host
-            Write-Host " ***************************"
-            Write-Host " *         Autopilot       *"
-            Write-Host " ***************************"
-            Write-Host
-            Write-Host " A login window will open, login with an Autopilot authorized user account"
-            Write-Host
-            try {
-                Get-WindowsAutopilotInfo.ps1 -online
-            } Catch {
-                Write-Host "Autopilot script not found, installing script"
-                install-script -Name Get-WindowsAutoPilotInfo -Force
-                Get-WindowsAutopilotInfo.ps1 -online
-            }
         }
         Q {
             Exit
