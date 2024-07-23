01e7535bb7976f76c6c32fbcc83af1c65e807dc5
JeffWantsToBattle <121041862+JeffWantsToBattle@users.noreply.github.com>
2024-07-23 14:55:00 +0200

    Update OSDCloudStartURL.ps1

--- OSDCloudStartURL.ps1
+++ OSDCloudStartURL.ps1
@@ -2,6 +2,29 @@
 $RepositoryURL  = '(Replace with RAW repository URL)'
 $ISOURL         = '(Replace with direct ISO download URL)'
 
+### Search OSDCloud and WinPE partitions
+$disk       = Get-WMIObject Win32_Volume | Where-Object { $_.Label -eq 'OSDCloudUSB' }
+$disk       = $disk.Name
+$diskwinpe  = Get-WMIObject Win32_Volume | Where-Object { $_.Label -eq 'WinPE' }
+$diskwinpe  = $diskwinpe.Name
+
+### Getting version from GitHub .\Update\Version.txt and .\Update\VersionWinPE.txt
+$version        = Invoke-WebRequest -Uri "$RepositoryURL/Update/Version.txt"
+$version        = $version.Content.Split([Environment]::NewLine) | Select-Object -First 1
+$versionWinPE   = Invoke-WebRequest -Uri $RepositoryURL/Update/VersionWinPE.txt
+$versionWinPE   = $versionWinPE.Content.Split([Environment]::NewLine) | Select-Object -First 1
+
+### Setting file names and locations
+$downloadspath  = (New-Object -ComObject Shell.Application).Namespace('shell:Downloads').Self.Path
+$file           = "Version.txt"
+$fileWinPE      = "VersionWinPE.txt"
+$folder         = 'OSDCloud\'
+$location       = "$disk$folder"
+
+### Getting versions from USB drive
+$versionondisk      = Get-Content "$location$file" -ErrorAction SilentlyContinue
+$versionWinPEondisk = Get-Content "$location$fileWinPE" -ErrorAction SilentlyContinue
+
 ### Install OSDCloud module if not present
 #if (Get-InstalledModule -Name OSD) { < Get-InstalledModule can be slow so replaced with Test-Path, need to test in WinPE
 if (Test-Path -Path "$env:ProgramFiles\WindowsPowerShell\Modules\OSD") {
@@ -36,7 +59,7 @@ Do {
     Switch ($Select)
         {
         1 {
-            Start-OSDCloudGUI
+            Invoke-WebPSScript $RepositoryURL/OSDCloudStartAndUpdate.ps1
         }
         2 {
             Invoke-WebPSScript $RepositoryURL/OSDCloudUpdateMenu.ps1
