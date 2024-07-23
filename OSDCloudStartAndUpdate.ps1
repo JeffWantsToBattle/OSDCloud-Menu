a3c2bc52b0dcb7aaa77c9875490b5faca04b068b
JeffWantsToBattle <121041862+JeffWantsToBattle@users.noreply.github.com>
2024-07-23 14:53:12 +0200

    Add files via upload

---
+++ OSDCloudStartAndUpdate.ps1
@@ -0,0 +1,36 @@
+Clear-Host
+Write-Host " ***************************"
+Write-Host " *      Start OSDCloud     *"
+Write-Host " ***************************"
+Write-Host
+if ($disk -eq $null) {
+    Write-host " OSDCloudUSB drive not found" -ForegroundColor Red
+    Write-host " Check that the partition name matches: OSDCloudUSB" -ForegroundColor Red
+    Write-Host
+} else {
+    ### Version check
+    if ($versionondisk -eq $version){
+        Write-host " OSDCloudUSB already updated to version $version" -ForegroundColor Green
+        Write-Host
+    } else {
+        Write-host " Updating OSDCloudUSB to version $version" -ForegroundColor Green
+        New-Item -ItemType Directory -Path $location\Automate -force -ErrorAction SilentlyContinue | Out-Null
+        ### Write new version + error handeling
+        try {
+            Invoke-WebRequest -Uri $RepositoryURL/Update/Automate/Start-OSDCloudGUI.json -OutFile $location\Automate\Start-OSDCloudGUI.json -UseBasicParsing
+        } catch {
+            Write-host " An error occurred: $($_.Exception.Message)" -ForegroundColor Red
+            $error1 = 1
+        }
+        if ($error1 -eq "1"){
+            Write-Host
+        } else {
+            New-Item -Path $location -Name "$file" -ItemType "file" -Value $version -Force -ErrorAction SilentlyContinue | Out-Null
+            Write-host " Updating compleet to version $version" -ForegroundColor Green
+            Write-Host
+        }
+    }
+}
+
+### Start OSDCloud GUI
+Start-OSDCloudGUI
