40e50cd3a390d599b147f3248fc642e5bc4a4f5b
JeffWantsToBattle <121041862+JeffWantsToBattle@users.noreply.github.com>
2024-07-23 14:05:01 +0200

    Update OSDCloudStartURL.ps1

--- OSDCloudStartURL.ps1
+++ OSDCloudStartURL.ps1
@@ -2,6 +2,29 @@
 $RepositoryURL  = 'https://raw.githubusercontent.com/JeffWantsToBattle/OSDCloud-Menu/main'
 $ISOURL         = 'https://jvdosd.blob.core.windows.net/bootimage/OSDCloud_NoPrompt.iso'
 
+### Search OSDCloud and WinPE partitions
+$disk       = Get-WMIObject Win32_Volume | Where-Object { $_.Label -eq 'OSDCloudUSB' }
+$disk       = $disk.Name
+$diskwinpe  = Get-WMIObject Win32_Volume | Where-Object { $_.Label -eq 'WinPE' }
+$diskwinpe  = $diskwinpe.Name
+
+### Getting version from GitHub .\Update\Version.txt and .\Update\VersionWinPE.txt
+$version        = Invoke-WebRequest -Uri "$RepositoryURL/Update/Version.txt"
+$version        = $version.Content.Split([Environment]::NewLine) | Select-Object -First 1
+$versionWinPE   = Invoke-WebRequest -Uri $RepositoryURL/Update/VersionWinPE.txt
+$versionWinPE   = $versionWinPE.Content.Split([Environment]::NewLine) | Select-Object -First 1
+
+### Setting file names and locations
+$DownloadsPath  = (New-Object -ComObject Shell.Application).Namespace('shell:Downloads').Self.Path
+$file           = "Version.txt"
+$fileWinPE      = "VersionWinPE.txt"
+$folder         = 'OSDCloud\'
+$location       = "$disk$folder"
+
+### Getting versions from USB drive
+$versionondisk      = Get-Content "$location$file" -ErrorAction SilentlyContinue
+$versionWinPEondisk = Get-Content "$location$fileWinPE" -ErrorAction SilentlyContinue
+
 ### Install OSDCloud module if not present
 #if (Get-InstalledModule -Name OSD) { < Get-InstalledModule can be slow so replaced with Test-Path, need to test in WinPE
 if (Test-Path -Path "$env:ProgramFiles\WindowsPowerShell\Modules\OSD") {
